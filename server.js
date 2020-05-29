821cec3ba41c84d7087ff95bfa0a4b43723f625d
rk1999 <rishabh.k1999@gmail.com>
2020-05-29 14:18:33 +0530

    mongoose unhandeled promise rejection resolved

--- server.js
+++ server.js
@@ -17,11 +17,8 @@ if(process.env.NODE_ENV !== 'production') {
 
 //Connect to Mongo
 mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to mongoose'))
-    // .then(() => console.log('MongoDB Connected.....'))
-    // .catch(err => console.log(err));
+    .then(() => console.log('MongoDB Connected.....'))
+    .catch(err => console.log(err));
 
 //EJS
 app.use(expressLayouts);
